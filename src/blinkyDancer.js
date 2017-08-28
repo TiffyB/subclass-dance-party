7c7538bbb35a000f0852c6121804350eef4787ac
Hack Reactor Student <student@hackreactor.com>
2017-08-28 16:39:16 -0700

    First two tests are passing.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,39 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var makeBlinkyDancer = function() {
+  makeDancer.call(this);
+
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
+  //this.oldStep = this.step; //what do we do with this???
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
 
-  return blinkyDancer;
+
+
 };
+
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+
+
+makeBlinkyDancer.prototype.step = function(timeBetweenSteps) {
+  // call the old version of step at the beginning of any call to this new version of step
+  //this.oldStep();
+  //debugger;
+  this.oldStep = this.$node;
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+  //setTimeout(this.step, 100);
+  //var context = this;
+  //makeDancer.prototype.step.bind(context);
+  console.log(this.step);
+  var context = this;
+  setTimeout(function() {
+    this.step.bind(context);
+
+  }, 100);
+
+};
+
